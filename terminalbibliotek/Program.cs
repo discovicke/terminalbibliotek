2bd96b7365d6cc8521574c4bde1cfda4136c06dc
Viktor Johansson <discovicke@gmail.com>
2026-02-19 14:08:56 +0100

    init

---
+++ terminalbibliotek/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace terminalbibliotek;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        using var connection = new SqlConnection("Server=localhost,1433;Database=terminalbibliotek;User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
+    }
+}
