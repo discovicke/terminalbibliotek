58ad0613bf6fdd57365d2c00328ac20f4fc68a23
Viktor Johansson <discovicke@gmail.com>
2026-02-19 14:34:55 +0100

    conn

--- terminalbibliotek/Program.cs
+++ terminalbibliotek/Program.cs
@@ -5,7 +5,18 @@ namespace terminalbibliotek;
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
         using var connection = new SqlConnection("Server=localhost,1433;Database=terminalbibliotek;User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
+        connection.Open();
+        var command = new SqlCommand(@"
+IF NOT EXISTS (
+SELECT * FROM sys.tables WHERE name = 'authors') 
+BEGIN
+CREATE TABLE authors (
+            id INT IDENTITY(1,1) PRIMARY KEY,
+            name NVARCHAR(50));
+END", connection);
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 }
