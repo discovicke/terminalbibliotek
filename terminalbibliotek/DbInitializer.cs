86cb9cce37df3449c4816ec3fd2fc1ca14ff8011
Viktor Johansson <discovicke@gmail.com>
2026-02-20 12:00:34 +0100

    Add DbInitializer class for database setup and refactor Program.cs to use it

---
+++ terminalbibliotek/DbInitializer.cs
@@ -0,0 +1,45 @@
+using System.Data.SqlClient;
+using Dapper;
+
+namespace terminalbibliotek;
+
+public class DbInitializer
+{
+    public bool Initialize(string connectionString)
+    {
+        using var connection = new SqlConnection(connectionString);
+
+        connection.Execute(@"
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'authors')
+BEGIN
+CREATE TABLE authors (
+ id INT IDENTITY(1,1) PRIMARY KEY,
+ name NVARCHAR(50),
+ birth_year INT)
+END");
+
+        connection.Execute(@"
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'books')
+BEGIN
+CREATE TABLE books (
+ id INT IDENTITY(1,1) PRIMARY KEY,
+ name NVARCHAR(50),
+ author_id INT REFERENCES authors(id),
+ published INT,
+ genre NVARCHAR(50)
+)
+END");
+
+        connection.Execute(@"
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'book_author')
+BEGIN
+CREATE TABLE book_author (
+ book_id INT REFERENCES books(id) ON DELETE CASCADE,
+ author_id INT REFERENCES authors(id) ON DELETE CASCADE,
+ PRIMARY KEY (book_id, author_id)
+)
+END");
+
+        return true;
+    }
+}
--- terminalbibliotek/Program.cs
+++ terminalbibliotek/Program.cs
@@ -10,81 +10,84 @@ class Program
         using var connection =
             new SqlConnection(
                 "Server=localhost,1433;Database=terminalbibliotek;User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
-        connection.Open();
-        var command = new SqlCommand(@"
-IF NOT EXISTS (
-SELECT * FROM sys.tables WHERE name = 'authors') 
-BEGIN
-CREATE TABLE authors (
-            id INT IDENTITY(1,1) PRIMARY KEY,
-            name NVARCHAR(50),
-            birth_year INT)
-END", connection);
-        command.ExecuteNonQuery();
-        connection.Close();
-
-
-        connection.Open();
-        var bookCommand = new SqlCommand(@"
-IF NOT EXISTS (
-SELECT * FROM sys.tables WHERE name = 'books') 
-BEGIN
-CREATE TABLE books (
-            id INT IDENTITY(1,1) PRIMARY KEY,
-            name NVARCHAR(50),
-            author_id INT REFERENCES authors(id),
-            published INT,
-            genre NVARCHAR(50))
-END", connection);
-        bookCommand.ExecuteNonQuery();
-        connection.Close();
-
-        connection.Open();
-        var bridgeCommand = new SqlCommand(@"
-IF NOT EXISTS (
-SELECT * FROM sys.tables WHERE name = 'book_author')
-BEGIN
-CREATE TABLE book_author (
-            book_id INT REFERENCES books(id) ON DELETE CASCADE,
-            author_id INT REFERENCES authors(id) ON DELETE CASCADE,
-            PRIMARY KEY (book_id, author_id))
-END", connection);
-        bridgeCommand.ExecuteNonQuery();
-        connection.Close();
-
-        if ((args[0] == "l" || args[0] == "list") && (args[1] == "a" || args[1] == "author"))
+        DbInitializer db = new DbInitializer();
+        if (!db.Initialize(connection.ConnectionString))
         {
-            if (args.Length > 2 && (args[2] == "--books" || args[2] == "-b"))
-            {
-                connection.Open();
-                var juncCommand = new SqlCommand(@"
-SELECT 
-    authors.name,
-b.name AS book_name
-FROM authors 
-    JOIN book_author ba ON authors.id = ba.author_id
-    JOIN books b ON ba.book_id = b.id", connection);
-                var juncReader = juncCommand.ExecuteReader();
+            Console.WriteLine("Databasen kunde inte initialiseras.");
+            return;
+        }
 
-                while (juncReader.Read())
+        switch (args[0].ToLowerInvariant())
+        {
+            case "l":
+            case "list":
+                if (args.Length > 1)
                 {
-                    Console.WriteLine($"{juncReader["name"]}: {juncReader["book_name"]}");
+                    if (args[1].ToLowerInvariant() == "a" || args[1].ToLowerInvariant() == "authors")
+                    {
+                        if (args.Length > 2 && (args[2] == "--books" || args[2] == "-b"))
+                        {
+                            connection.Open();
+                            var bookAndAuthors = connection.Query(@"
+                                SELECT 
+                                a.name, 
+                                b.name AS book_name
+                                FROM book_author ba
+                                JOIN authors a ON ba.author_id = a.id
+                                JOIN books b ON ba.book_id = b.id
+                                ");
+                            foreach (var author in bookAndAuthors)
+                            {
+                                Console.WriteLine($"{author.name}: {author.book_name}");
+                            }
+                            connection.Close();
+                            return;
+                        }
+                        connection.Open();
+                        var authors = connection.Query("SELECT * FROM authors");
+                        foreach (var author in authors)
+                        {
+                            Console.WriteLine($"{author.id}: {author.name} | {author.birth_year}");
+                        }
+
+                        connection.Close();
+                        return;
+                    }
+
+                    if (args[1].ToLowerInvariant() == "b" || args[1].ToLowerInvariant() == "books")
+                    {
+                        if (args.Length > 2 && (args[2] == "--authors" || args[2] == "-a"))
+                        {
+                            connection.Open();
+                            var authorAndBooks = connection.Query(@"
+                                SELECT 
+                                b.name, 
+                                a.name AS author_name
+                                FROM book_author ba
+                                JOIN books b ON b.id = ba.book_id
+                                JOIN authors a ON ba.author_id = a.id
+                                ");
+                            foreach (var book in authorAndBooks)
+                            {
+                                Console.WriteLine($"{book.name}: {book.author_name}");
+                            }
+                            connection.Close();
+                            return;
+                        }
+                        connection.Open();
+                        var listCommand = connection.Query("SELECT * FROM books");
+                        foreach (var book in listCommand)
+                        {
+                            Console.WriteLine($"{book.id}: {book.name} | {book.published} | {book.genre}");
+                        }
+                        connection.Close();
+                        return;
+                    }
                 }
 
-                connection.Close();
-
+                Console.WriteLine("Ogiltigt kommando. Använd 'list authors' eller 'list books'.");
                 return;
-            }
-
-            connection.Open();
-            var listCommand = new SqlCommand("SELECT * FROM authors", connection);
-            var reader = listCommand.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader["id"]}: {reader["name"]}");
-            }
-
-            connection.Close();
+            
         }
 
         if ((args[0] == "a" && args[1] == "a") || (args[0] == "add" && args[1] == "author"))
@@ -145,6 +148,7 @@ FROM book_author ba
                 {
                     Console.WriteLine($"{juncReader["name"]}: {juncReader["author_name"]}");
                 }
+
                 connection.Close();
                 return;
             }
@@ -160,7 +164,7 @@ FROM book_author ba
 
             connection.Close();
         }
-        
+
         if ((args[0] == "m" && args[1] == "a") && (args[3] == "s" && args[4] == "b") ||
             (args[0]) == "modify" && args[1] == "author" && args[3] == "set" && args[4] == "born")
         {
@@ -173,14 +177,14 @@ FROM book_author ba
             {
                 return;
             }
-            
+
             if (authResult == null)
             {
                 Console.WriteLine($"Författare '{args[2]}' hittades inte!");
                 connection.Close();
                 return;
             }
-            
+
             var addAuthorCommand = new SqlCommand(@"UPDATE authors 
     SET birth_year = @birth_year
     WHERE name = @name", connection);
@@ -189,7 +193,7 @@ FROM book_author ba
             addAuthorCommand.ExecuteNonQuery();
             connection.Close();
         }
-        
+
         if ((args[0] == "m" && args[1] == "a" && args[3] == "a" && args[4] == "b") ||
             (args[0] == "modify" && args[1] == "author" && args[3] == "add" && args[4] == "book"))
         {
@@ -274,7 +278,7 @@ FROM book_author ba
             {
                 return;
             }
-            
+
             connection.Open();
             var booklistCommand = new SqlCommand("SELECT id FROM books WHERE name = @name", connection);
             booklistCommand.Parameters.AddWithValue("@name", args[2]);
@@ -286,7 +290,7 @@ FROM book_author ba
                 connection.Close();
                 return;
             }
-            
+
             var bookId = Convert.ToInt32(bookResult);
             var updateBookCommand = new SqlCommand(@"UPDATE books 
     SET published = @published
@@ -296,7 +300,7 @@ FROM book_author ba
             updateBookCommand.ExecuteNonQuery();
             connection.Close();
         }
-        
+
         if ((args[0] == "m" && args[1] == "b" && args[3] == "s" && args[4] == "g") ||
             (args[0] == "modify" && args[1] == "book" && args[3] == "set" && args[4] == "genre"))
         {
@@ -311,7 +315,7 @@ FROM book_author ba
                 connection.Close();
                 return;
             }
-            
+
             var updateBookCommand = new SqlCommand(@"UPDATE books 
     SET genre = @genre
     WHERE name = @name", connection);
